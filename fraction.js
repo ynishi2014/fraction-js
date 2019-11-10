60a6f94cc728271775fb2b303e8249947314e987
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2019-11-10 19:03:54 +0900

    Update fraction.js
    インデントを修正／整数を分数に変換する関数を追加／分数を実数として取り出す関数を追加

--- fraction.js
+++ fraction.js
@@ -9,15 +9,21 @@ function gcd(x, y) {
   return x;
 }
 function reduce(num){
-    var gcdNum = gcd(num[0], num[1]);
-    return [num[0]/gcdNum, num[1]/gcdNum];
+  var gcdNum = gcd(num[0], num[1]);
+  return [num[0]/gcdNum, num[1]/gcdNum];
 }
 function mul(num1, num2){
-    return reduce([num1[0]*num2[0], num1[1]*num2[1]]);
+  return reduce([num1[0]*num2[0], num1[1]*num2[1]]);
 }
 function div(num1, num2){
-    return reduce([num1[0]*num2[1], num1[1]*num2[0]]);
+  return reduce([num1[0]*num2[1], num1[1]*num2[0]]);
 }
 function add(num1, num2){
-    return reduce([num1[0]*num2[0], num1[0]*num2[1] + num2[0] * num1[1]]);
+  return reduce([num1[0]*num2[0], num1[0]*num2[1] + num2[0] * num1[1]]);
+}
+function tofrac(n){
+  return [1,n];
+}
+function toDouble(num){
+  return num[1] / num[0];
 }
