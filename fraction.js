7c15284a500900bb3e6ddd6a6ea04de0ad239580
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2019-11-10 18:49:33 +0900

    Create fraction.js
    分数計算を実装

---
+++ fraction.js
@@ -0,0 +1,23 @@
+function gcd(x, y) {
+  x = Math.abs(x);
+  y = Math.abs(y);
+  while(y) {
+    var t = y;
+    y = x % y;
+    x = t;
+  }
+  return x;
+}
+function reduce(num){
+    var gcdNum = gcd(num[0], num[1]);
+    return [num[0]/gcdNum, num[1]/gcdNum];
+}
+function mul(num1, num2){
+    return reduce([num1[0]*num2[0], num1[1]*num2[1]]);
+}
+function div(num1, num2){
+    return reduce([num1[0]*num2[1], num1[1]*num2[0]]);
+}
+function add(num1, num2){
+    return reduce([num1[0]*num2[0], num1[0]*num2[1] + num2[0] * num1[1]]);
+}
