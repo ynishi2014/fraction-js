7ee5a28df3dc317d3085956311bdbe94f47f6171
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2019-11-11 15:10:17 +0900

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,30 +1,34 @@
-  var fraction = {}
-  fraction.gcd = function (x, y) {
-    x = Math.abs(x);
-    y = Math.abs(y);
-    while (y) {
-      var t = y;
-      y = x % y;
-      x = t;
-    }
-    return x;
+/**
+ * 分数計算ライブラリ
+ * @author yoichiro
+ */
+var fraction = {}
+fraction.gcd = function (x, y) {
+  x = Math.abs(x);
+  y = Math.abs(y);
+  while (y) {
+    var t = y;
+    y = x % y;
+    x = t;
   }
-  fraction.reduce = function (num) {
-    var gcdNum = gcd(num[0], num[1]);
-    return [num[0] / gcdNum, num[1] / gcdNum];
-  }
-  fraction.mul = function (num1, num2) {
-    return reduce([num1[0] * num2[0], num1[1] * num2[1]]);
-  }
-  fraction.div = function (num1, num2) {
-    return reduce([num1[0] * num2[1], num1[1] * num2[0]]);
-  }
-  fraction.add = function (num1, num2) {
-    return reduce([num1[0] * num2[0], num1[0] * num2[1] + num2[0] * num1[1]]);
-  }
-  fraction.tofrac = function (n) {
-    return [1, n];
-  }
-  fraction.toDouble = function (num) {
-    return num[1] / num[0];
-  }
+  return x;
+}
+fraction.reduce = function (num) {
+  var gcdNum = fraction.gcd(num[0], num[1]);
+  return [num[0] / gcdNum, num[1] / gcdNum];
+}
+fraction.mul = function (num1, num2) {
+  return fraction.reduce([num1[0] * num2[0], num1[1] * num2[1]]);
+}
+fraction.div = function (num1, num2) {
+  return fraction.reduce([num1[0] * num2[1], num1[1] * num2[0]]);
+}
+fraction.add = function (num1, num2) {
+  return fraction.reduce([num1[0] * num2[0], num1[0] * num2[1] + num2[0] * num1[1]]);
+}
+fraction.tofrac = function (n) {
+  return [1, n];
+}
+fraction.toDouble = function (num) {
+  return num[1] / num[0];
+}
